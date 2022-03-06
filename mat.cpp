b42d54d6a25f10219e58da856ec34e27c30e22ed
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-03-06 15:05:03 +0200

    Tests before implementation

---
+++ mat.cpp
@@ -0,0 +1,13 @@
+//
+// Created by shira on 06/03/2022.
+//
+#include "mat.hpp"
+#include <string>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+//using namespace ariel;
+
+string ariel::mat (int r, int c, char c1, char c2){
+    return "123";
+}
