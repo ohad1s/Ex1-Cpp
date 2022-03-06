1af60fe1dd8a1fec39b1e3784f60a0f73d44497e
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-03-06 22:13:20 +0200

    starting implementation

--- mat.cpp
+++ mat.cpp
@@ -9,5 +9,19 @@ using namespace std;
 //using namespace ariel;
 
 string ariel::mat (int r, int c, char c1, char c2){
+    if (r<=0 || c<=0){
+        throw "zero or negative input error ";
+    }
+    if (r%2==0 || c%2==0){
+        throw "even input error";
+    }
+    char** my_mat=new char*[r];
+    for (int i=0; i<r; i++){
+        my_mat[i]=new char[c];
+    }
+    int start=0, row=r-1, col=c-1;
+    while (start<r || start<c){
+        for (int i=0; i<r;)
+    }
     return "123";
 }
