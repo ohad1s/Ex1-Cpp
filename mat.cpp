85e2601c0640fc72ceb624a853f045e5be740cd1
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-03-09 17:23:36 +0200

    implementation

--- mat.cpp
+++ mat.cpp
@@ -5,23 +5,69 @@
 #include <string>
 #include <iostream>
 #include <stdexcept>
+
 using namespace std;
 //using namespace ariel;
 
-string ariel::mat (int r, int c, char c1, char c2){
-    if (r<=0 || c<=0){
+string ariel::mat(int r, int c, char c1, char c2) {
+    if (r <= 0 || c <= 0) {
         throw "zero or negative input error ";
     }
-    if (r%2==0 || c%2==0){
-        throw "even input error";
+//    if (r % 2 == 0 || c % 2 == 0) {
+//        throw "Mat size is always odd";
+//    }
+    char **my_mat = new char *[r];
+    for (int i = 0; i < r; i++) {
+        my_mat[i] = new char[c];
     }
-    char** my_mat=new char*[r];
-    for (int i=0; i<r; i++){
-        my_mat[i]=new char[c];
+
+    int start = 0, row = r-1 , col = c-1 ;
+    char sign = c1;
+
+    while (start <= row && start <= col) {
+
+        for (int i = start; i <= row; i++) {
+            my_mat[i][start] = sign;
+            my_mat[i][col]= sign;
+        }
+
+        for (int j = start; j <= col; j++) {
+            my_mat[start][j] = sign;
+            my_mat[row][j] = sign;
+        }
+        row = row - 1;
+        col = col - 1;
+        start = start + 1;
+        if (sign == c1) {
+            sign = c2;
+        } else {
+            sign = c1;
+        }
     }
-    int start=0, row=r-1, col=c-1;
-    while (start<r || start<c){
-        for (int i=0; i<r;)
+    string str_mat = "";
+    bool flag = true;
+    for (int i = 0; i < r; i++) {
+        if (i == r - 1) {
+            flag = false;
+        }
+        for (int j = 0; j < c; j++) {
+            str_mat = str_mat + my_mat[i][j];
+        }
+        if (flag == true) {
+            str_mat = str_mat + "\n";
+        }
+
     }
-    return "123";
+    for (int i = 0; i < r; i++) {
+        delete[]my_mat[i];
+    }
+    delete[] my_mat;
+    return str_mat;
 }
+
+//int main() {
+//
+//    std::cout << ariel::mat(11, 7, '1', '0') << endl;
+//
+//    return 0;
+//}
